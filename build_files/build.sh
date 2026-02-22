3aaaab4ebaa99bc5918ef67ab209bda18d3b5761
murdzheff <murdzheff@protonmail.com>
2026-02-23 01:20:18 +0200

    add portals

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux brightnessctl lxappearance thunar grim slurp clipman lxpolkit sway swaybg swayidle foot fuzzel dunst wlsunset 
-
+dnf5 install -y xdg-desktop-portal-gtk xdg-desktop-portal-wlr
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
