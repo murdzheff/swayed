76cdfef75980e348aefcdf92c83c56cfc225ca31
murdzheff <murdzheff@protonmail.com>
2026-02-23 00:51:05 +0200

    add polkit

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux sway swaybg swayidle foot fuzzel dunst 
+dnf5 install -y tmux lxpolkit sway swaybg swayidle foot fuzzel dunst wlsunset 
 
 # Use a COPR Example:
 #
