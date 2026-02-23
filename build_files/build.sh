be50d0107330bcd278b074767dcf8173f01369ed
murdzheff <murdzheff@protonmail.com>
2026-02-23 10:03:48 +0200

    more packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux brightnessctl lxappearance thunar grim slurp clipman lxpolkit sway swaybg swayidle foot fuzzel dunst wlsunset 
-dnf5 install -y waybar qutebrowser NetworkManager-tui blueman  xdg-desktop-portal-gtk xdg-desktop-portal-wlr wl-clipboard
+dnf5 install -y waybar python3-adblock qutebrowser NetworkManager-tui blueman  xdg-desktop-portal-gtk xdg-desktop-portal-wlr wl-clipboard
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
